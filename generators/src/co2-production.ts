3b0f868fe31450538c9d4941cf91f18714947ff0
clerici-projectwork <clerici.projectwork.pegaso@gmail.com>
2025-04-05 00:02:37 +0200

    update generator files

--- generators/src/co2-production.ts
+++ generators/src/co2-production.ts
@@ -3,7 +3,10 @@ import { join } from 'node:path';
 import {
   CO2_TONS_PRODUCTION_PER_COPPER_TON,
   CO2_TONS_PRODUCTION_PER_GOLD_TON,
-  CO2_TONS_PRODUCTION_PER_SILVER_TON
+  CO2_TONS_PRODUCTION_PER_SILVER_TON,
+  COPPER_TONS_DAILY_PRODUCTION,
+  GOLD_TONS_DAILY_PRODUCTION,
+  SILVER_TONS_DAILY_PRODUCTION
 } from '@clerici/common/constants';
 import type { TCo2ProductionByMetalGeneratedJson } from '@clerici/common/types';
 import { gaussianDistribution } from '@clerici/common/utils';
@@ -24,13 +27,16 @@ function generateCO2Production() {
   // thus we need these amount of data
   for (let i = 0; i < 365; i++) {
     co2Production.gold.push(
-      gaussianDistribution(CO2_TONS_PRODUCTION_PER_GOLD_TON, CO2_TONS_PRODUCTION_PER_GOLD_TON / 10)
+      gaussianDistribution(CO2_TONS_PRODUCTION_PER_GOLD_TON, CO2_TONS_PRODUCTION_PER_GOLD_TON / 10) *
+      gaussianDistribution(GOLD_TONS_DAILY_PRODUCTION, GOLD_TONS_DAILY_PRODUCTION / 10)
     );
     co2Production.silver.push(
-      gaussianDistribution(CO2_TONS_PRODUCTION_PER_SILVER_TON, CO2_TONS_PRODUCTION_PER_SILVER_TON / 10)
+      gaussianDistribution(CO2_TONS_PRODUCTION_PER_SILVER_TON, CO2_TONS_PRODUCTION_PER_SILVER_TON / 10) *
+      gaussianDistribution(SILVER_TONS_DAILY_PRODUCTION, SILVER_TONS_DAILY_PRODUCTION / 10)
     );
     co2Production.copper.push(
-      gaussianDistribution(CO2_TONS_PRODUCTION_PER_COPPER_TON, CO2_TONS_PRODUCTION_PER_COPPER_TON / 10)
+      gaussianDistribution(CO2_TONS_PRODUCTION_PER_COPPER_TON, CO2_TONS_PRODUCTION_PER_COPPER_TON / 10)*
+      gaussianDistribution(COPPER_TONS_DAILY_PRODUCTION, COPPER_TONS_DAILY_PRODUCTION / 10)
     );
   }
 
--- generators/src/metals-production-cost.ts
+++ generators/src/metals-production-cost.ts
@@ -1,9 +1,12 @@
 import { existsSync, unlinkSync, writeFileSync } from 'node:fs';
 import { join } from 'node:path';
 import {
+  COPPER_GR_PRODUCTION_COST,
   COST_COPPER_EXTRACTION_PER_TON,
   COST_GOLD_EXTRACTION_PER_TON,
-  COST_SILVER_EXTRACTION_PER_TON
+  COST_SILVER_EXTRACTION_PER_TON,
+  GOLD_TONS_DAILY_PRODUCTION,
+  SILVER_TONS_DAILY_PRODUCTION
 } from '@clerici/common/constants';
 import type { TMetalProductionCostGeneratedJson } from '@clerici/common/types';
 import { gaussianDistribution } from '@clerici/common/utils';
@@ -24,13 +27,16 @@ function generateMetalsProductionCost() {
   // thus we need these amount of data
   for (let i = 0; i < 365; i++) {
     metalsProductionCost.gold.push(
-      gaussianDistribution(COST_GOLD_EXTRACTION_PER_TON, COST_GOLD_EXTRACTION_PER_TON / 10)
+      gaussianDistribution(COST_GOLD_EXTRACTION_PER_TON, COST_GOLD_EXTRACTION_PER_TON / 10)*
+            gaussianDistribution(GOLD_TONS_DAILY_PRODUCTION, GOLD_TONS_DAILY_PRODUCTION / 10)
     );
     metalsProductionCost.silver.push(
-      gaussianDistribution(COST_SILVER_EXTRACTION_PER_TON, COST_SILVER_EXTRACTION_PER_TON / 10)
+      gaussianDistribution(COST_SILVER_EXTRACTION_PER_TON, COST_SILVER_EXTRACTION_PER_TON / 10)*
+      gaussianDistribution(SILVER_TONS_DAILY_PRODUCTION, SILVER_TONS_DAILY_PRODUCTION / 10)
     );
     metalsProductionCost.copper.push(
-      gaussianDistribution(COST_COPPER_EXTRACTION_PER_TON, COST_COPPER_EXTRACTION_PER_TON / 10)
+      gaussianDistribution(COST_COPPER_EXTRACTION_PER_TON, COST_COPPER_EXTRACTION_PER_TON / 10)*
+      gaussianDistribution(COPPER_GR_PRODUCTION_COST, COPPER_GR_PRODUCTION_COST / 10)
     );
   }
 
